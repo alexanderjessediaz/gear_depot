c89161bfca796d4cccfaf1a25441a30287a66002
AJ <alexanderjessediaz@gmail.com>
2021-07-29 16:30:22 -0600

    reconfigure postcss to use tailwind

---
+++ app/javascript/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class NavBar extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h2>test</h2>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar
