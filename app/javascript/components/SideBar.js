cb6656b326e6b3a2ef7b5d16d6084283476d9a41
AJ <alexanderjessediaz@gmail.com>
2021-07-30 13:10:16 -0600

    style sidebar links

--- app/javascript/components/SideBar.js
+++ app/javascript/components/SideBar.js
@@ -5,19 +5,19 @@ export default function SideBar() {
         <>
             <div class="relative min-h-screen flex">
 
-                <div class="bg-blue-800 text-blue-100 w-64 space-y-6">
+                <div class="bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2">
 
-                    <a href="#" class="text-white flex items-center space-x-2"/>
+                    <a href="#" class="text-white flex items-center space-x-2 px-4"/>
                     <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                     </svg>
                     
                     <span class="text-2x1 font-extrabold">Gear Repository</span>
                     <nav>
-                        <a href="#" class="block py-2 5 px-4">Home</a>
-                        <a href="#" class="block py-2 5 px-4">About</a>
-                        <a href="#" class="block py-2 5 px-4">Features</a>
-                        <a href="#" class="block py-2 5 px-4">Pricing</a>
+                        <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">Home</a>
+                        <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">About</a>
+                        <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">Features</a>
+                        <a href="#" class="block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white">Pricing</a>
                     </nav>    
                 </div>
             
