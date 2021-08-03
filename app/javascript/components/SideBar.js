62ebf780277b412daa3c0865cf8fcb6e73d279ef
AJ <alexanderjessediaz@gmail.com>
2021-08-03 12:23:30 -0600

    make sidebar responsive

--- app/javascript/components/SideBar.js
+++ app/javascript/components/SideBar.js
@@ -3,9 +3,19 @@ import React from 'react'
 export default function SideBar() {
     return(
         <>
-            <div class="relative min-h-screen flex">
+            <div class="relative min-h-screen md:flex">
 
-                <div class="bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2">
+                <div class="bg-gray-800 text-gray-100 flex justify-between md:hidden">
+                    <a href="#" class="block p-4 text-white font-bold">Gear Repository</a>
+
+                    <button class="mobile-menu-button p-4 focus:outline-none focus:bg-gray-700">
+                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>  
+                    </button>
+                </div>
+
+                <div class="sidebar bg-blue-800 text-blue-100 w-64 space-y-6 py-7 px-2 absolute inset-y-0 left-0 transform -translate-x-full  md:relative md:translate-x-0 transition duration-200 ease-in-out">
 
                     <a href="#" class="text-white flex items-center space-x-2 px-4"/>
                     <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -22,9 +32,8 @@ export default function SideBar() {
                 </div>
             
             </div>
-
+            {/* main-console */}
             <div>
-
 
             </div>
         </>
