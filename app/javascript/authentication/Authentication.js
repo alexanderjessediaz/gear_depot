baa183779a399b605b2d3d52f512a12d694393b6
AJ <alexanderjessediaz@gmail.com>
2021-08-17 13:39:13 -0600

    create wire frame for Auth component

---
+++ app/javascript/authentication/Authentication.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Home from '../../javascript/components/Home'
+
+export default class Authentication extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+        }
+        
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get "logged_in",  to: "sessions#logged_in"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get '*path', to: 'home#index'
 end
